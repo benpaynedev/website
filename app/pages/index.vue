3e2ba0398ca9c131cbdae4f48a2b9afda2a82be4
Ben Payne <bj.payne@me.com>
2026-02-08 14:34:53 -0500

    dev commit

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -20,6 +20,8 @@ const { defineField, errorBag, handleSubmit, resetForm } = useForm({
 
 let currentPage = ref('');
 let sending = ref(false);
+let sent = ref(false);
+let sendButtonText = ref('Send');
 let config = useRuntimeConfig();
 
 let [name, nameAttrs] = defineField('name');
@@ -28,6 +30,7 @@ let [message, messageAttrs] = defineField('message');
 
 const onSubmit = handleSubmit(values => {
   sending.value = true;
+  sendButtonText.value = 'Sending...';
 
   const formData = new FormData()
 
@@ -43,12 +46,17 @@ const onSubmit = handleSubmit(values => {
     },
     body: formData,
   }).then(() => {
-    toast.add({
-      description: 'Your message has been sent.',
-      progress: false
-    });
+    sent.value = true;
+    sendButtonText.value = 'Sent!';
 
-    sending.value = false;
+    setTimeout(() => {
+      sendButtonText.value = 'Send';
+    }, 2500);
+
+    setTimeout(() => {
+      sent.value = false;
+      sending.value = false;
+    }, 5000);
     resetForm();
   });
 });
@@ -136,22 +144,10 @@ onMounted(() => {
 </script>
 
 <style scoped>
-  main {
-    border: 2px solid #fff;
-    border-radius: 0.5rem;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    position: absolute;
-    top: 0; right: 0; bottom: 0; left: 0;
-    margin: auto;
-    height: 100px;
-    width: 200px;
-  }
+
 </style>
 
 <template>
-
 <!-- Cursor glow -->
 <div class="cursor-glow" id="cursorGlow"></div>
 
@@ -333,7 +329,7 @@ onMounted(() => {
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg>
           GitHub
         </a>
-        <a href="https://bold.pro/my/ben-payne-250924114209" target="_blank" class="contact-link magnetic">
+        <a href="/Benjamin_Payne_Resume.pdf" target="_blank" class="contact-link magnetic">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" /></svg>
           Resume
         </a>
@@ -368,8 +364,13 @@ onMounted(() => {
           Please enter your message
         </p>
       </div>
-      <button type="submit" class="btn btn-primary btn-submit magnetic" :disabled="sending" >
-        <span v-text="sending ? 'Sending...' : 'Send'"></span>
+      <button type="submit" class="btn btn-primary btn-submit magnetic" :class="{submitted: sent}" :disabled="sending" >
+        <span>
+          <span>S</span>
+          <span>e</span>
+          <span>n</span>
+          <span>d</span>
+        </span>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" /></svg>
       </button>
     </form>
